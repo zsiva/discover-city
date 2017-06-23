c4f0f742fb5f315632961ef56978c95c55b7d2c5
Marta <m.carrizo.ibarra@gmail.com>
2017-06-23 15:49:04 +0200

    update cities

--- src/components/CityLayout/index.js
+++ src/components/CityLayout/index.js
@@ -2,40 +2,63 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 
 import Button from '../Button';
+import './style.css';
 
-//import './style.css';
+const CityRow = (props) =>
+  <div className="col-4 text-center">
+    <Button label={props.label} onClick={props.handleClick}/>
+  </div>
+
+class HintRow extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {hidden: 'hidden'}
+  }
+  componentWillMount () {
+      setTimeout(() => {
+        this.setState({hidden : ""});
+      }, this.props.wait);
+  }
+  render(){
+    const {label} = this.props;
+
+    return (
+        <p style={{"visibility": this.state.hidden}}>{label}</p>
+    )
+  }
+};
 
 class CityLayout extends Component {
   constructor(props) {
     super(props);
 
     this.handleClick = this.handleClick.bind(this);
-    this.state = {cities: []};
   }
 
-  handleClick(){
-
-    console.log('test', this.state);
+  handleClick(event){
+    if ( event.currentTarget.innerText === 'Berlin') {
+      console.log('FOUND');
+    }
   }
 
   render() {
+    const {cities} = this.props;
+    const hints = ['Buying a beer in a Späti near Kottbuser Tor', 'Second', 'Third']
 
     return (
       <div className="container">
         <div className="row">
+          <div className="col-12 hints-container">
+            <p>The thief was last seen ...</p>
+            {hints.map((hint, i) => <HintRow key={i} label={hint} wait={(i+1) * 1000}/>)}
+          </div>
+        </div>
+        <div className="row">
           <div className="col-12">
-            <h3>Select City</h3>
+            <h3>Where is he?</h3>
           </div>
           <div className="row">
-            <div className="col-4 text-center">
-              <Button label="Berlin" onClick={this.handleClick}/>
-            </div>
-            <div className="col-4 text-center">
-              <Button label="London" onClick={this.handleClick}/>
-            </div>
-            <div className="col-4 text-center">
-              <Button label="Paris" onClick={this.handleClick}/>
-            </div>
+            { cities.map((city, index) => <CityRow key={index} label={city} handleClick={this.handleClick} />)}
           </div>
         </div>
       </div>
@@ -43,14 +66,13 @@ class CityLayout extends Component {
   }
 }
 
-export default CityLayout;
-/*
-const mapStateToProps = (state, ownProps) => {
-  return
-  {cities: ['London', 'Berlin', 'Paris', 'Madrid']}
-};
-
+const mapStateToProps = (state, ownProps = {}) => {
+  return {
+    cities: state.gameState.cities,
+    currentCity: state.gameState.currentCity
+  }
+}
 
 export default connect(
   mapStateToProps,
-)(CityLayout)*/
+)(CityLayout)
