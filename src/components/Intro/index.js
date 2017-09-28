06e51721a5b7a091eb7cb8cb8878e980f0e0d97c
Marta <m.carrizo.ibarra@gmail.com>
2017-09-28 11:08:06 +0200

    update Intro layout

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -8,22 +8,16 @@ class Intro extends Component {
     const {handleClick} = this.props;
     return (
       <section className="container">
+        <h1 className="text-center visible-xs">Help O&apos;Greeny</h1>
         <div className="row">
-          <div className="col-xs-12">
-            <img className="leprechaun" src="./images/ogreeny.png" alt=""/>
-            <h1>OGreeny is looking for his gold.</h1>
-            <h2>Where should he go next?</h2>
+          <div className="col-sm-4 text-center">
+            <img className="leprechaun" src="./images/ogreeny2.png" alt="ogreeny"/>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-xs-12">
-            <p>Our little lepprechaun Ogreeny was chilling, having a cold Guiness while his money was stolen!</p>
-            <p>He has been looking all over the world for it but cannot find it. Help him!</p>
-          </div>
-          <div className="row">
-            <div className="col-xs-12 text-center">
+          <div className="col-xs-12 col-sm-8 text-center">
+            <h1 className="hidden-xs">Help O&apos;Greeny</h1>
+            <p className="text-left">Our little lepprechaun Ogreeny was chilling, having a cold Guiness while his money was stolen!</p>
+            <p className="text-left">He has been looking all over the world for it but cannot find it. Help him!</p>
             <Button label="Find the gold!" onClick={handleClick}/>
-            </div>
           </div>
         </div>
       </section>
