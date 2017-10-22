f9310c46bb6abdd35e1c59ae335def52771212d8
Marta Carrizo <m.carrizo.ibarra@gmail.com>
2017-10-22 11:30:22 +0200

    add service worker

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
-import Root from './components/Root';
-import './index.css';
+import Root from "./components/Root";
+import "./index.css";
 
-import configureStore from './configure-store';
+import configureStore from "./configure-store";
+import registerServiceWorker from "./registerServiceWorker";
 
 const initialState = {
   timer: {
-     isOn: false,
-     time: 15,
-     interval: null
-   },
-   gameState: {
-     currentCityID: 0,
-     gameEnded: false
-   }
+    isOn: false,
+    time: 15,
+    interval: null
+  },
+  gameState: {
+    currentCityID: 0,
+    gameEnded: false
+  }
 };
 
 const store = configureStore(initialState);
@@ -25,5 +26,6 @@ ReactDOM.render(
   <Provider store={store}>
     <Root />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+registerServiceWorker();
