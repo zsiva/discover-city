996de1c69de68c4d83ada2c569bb43a92459d441
Marta Carrizo <m.carrizo.ibarra@gmail.com>
2018-03-24 12:12:12 +0100

    Reorder flag images

--- src/data/cities.js
+++ src/data/cities.js
@@ -1,146 +1,146 @@
 export const CITIES = [
-   {
+  {
     name: 'Berlin',
     police: 'POLIZEI',
     policeimg: 'policeDE.png',
-	flag: 'GERM0001.GIF',
-    food:'Bratwurst',
+    flag: 'countryFlags/GERM0001.GIF',
+    food: 'Bratwurst',
     hints: [
       {
         label: 'Walking around the Cathedral',
-        img: 'images/cities/berliner-dom.jpg'
+        img: 'images/cities/berliner-dom.jpg',
       },
       {
         label: 'Chilling at TierGarten',
-        img: 'images/cities/tiergarten.jpg'
+        img: 'images/cities/tiergarten.jpg',
       },
       {
         label: 'Crossing the Oberbaumbrücke',
-        img: 'images/cities/oberbaum.jpg'
+        img: 'images/cities/oberbaum.jpg',
       },
       {
         label: 'Going up the Fernsehturm',
-        img: 'images/cities/fernsehturm.jpg'
-      }
+        img: 'images/cities/fernsehturm.jpg',
+      },
     ],
-    cityOptions: ['Berlin', 'London', 'Budapest']
-    },
-    {
+    cityOptions: ['Berlin', 'London', 'Budapest'],
+  },
+  {
     name: 'Paris',
     police: 'GENDARMERIE',
     policeimg: 'policeFR.png',
-	flag: 'FRAN0001.GIF',	  
-    food:'Croissant',
+    flag: 'countryFlags/FRAN0001.GIF',
+    food: 'Croissant',
     hints: [
-        {
-          label: 'In a shop near Notre Dame',
-          img: 'images/cities/notre-dame.jpg'
-        },
-        {
-          label: 'Looking at the view from Montmartre',
-          img: 'images/cities/retiro.jpg'
-        },
-        {
-          label: 'Going up the Eiffel Tower',
-          img: 'images/cities/tour-eiffel.jpg'
-        }
-      ],
-      cityOptions: ['Viena', 'Paris', 'Luxemburg']
-    },
-    {
+      {
+        label: 'In a shop near Notre Dame',
+        img: 'images/cities/notre-dame.jpg',
+      },
+      {
+        label: 'Looking at the view from Montmartre',
+        img: 'images/cities/retiro.jpg',
+      },
+      {
+        label: 'Going up the Eiffel Tower',
+        img: 'images/cities/tour-eiffel.jpg',
+      },
+    ],
+    cityOptions: ['Viena', 'Paris', 'Luxemburg'],
+  },
+  {
     name: 'Rome',
     police: 'CARABINIERI',
     policeimg: 'policeIT.png',
-	flag: 'ITAL0001.GIF',  		
-    food:'Pizza',
-      hints: [
-        {
-          label: 'Making pictures at the Colosseum',
-          img: 'images/cities/colosseum.jpg'
-        },
-        {
-          label: 'Walking around the Circus Maximus',
-          img: 'images/cities/retiro.jpg'
-        },
-        {
-          label: 'Visiting the Temple of Jupiter',
-          img: 'images/cities/retiro.jpg'
-        },
-        {
-          label: 'Around the Vatican',
-          img: 'images/cities/vatican.jpg'
-        }
-      ],
-      cityOptions: ['Helsinki', 'Nantes', 'Rome']
-    },
-    {
+    flag: 'countryFlags/ITAL0001.GIF',
+    food: 'Pizza',
+    hints: [
+      {
+        label: 'Making pictures at the Colosseum',
+        img: 'images/cities/colosseum.jpg',
+      },
+      {
+        label: 'Walking around the Circus Maximus',
+        img: 'images/cities/retiro.jpg',
+      },
+      {
+        label: 'Visiting the Temple of Jupiter',
+        img: 'images/cities/retiro.jpg',
+      },
+      {
+        label: 'Around the Vatican',
+        img: 'images/cities/vatican.jpg',
+      },
+    ],
+    cityOptions: ['Helsinki', 'Nantes', 'Rome'],
+  },
+  {
     name: 'Madrid',
     police: 'GUARDIA CIVIL',
     policeimg: 'policeES.png',
-	flag: 'SPAN0001.GIF',	  		
-    food:'Paella',
+    flag: 'countryFlags/SPAN0001.GIF',
+    food: 'Paella',
     hints: [
-        {
-          label: 'Making a picnic in the Retiro Park',
-          img: 'images/cities/retiro.jpg'
-        },
-        {
-          label: 'Admiring the paintings in the Prado Museum',
-          img: 'images/cities/prado.jpg'
-        },
-        {
-          label: 'Eating some tapas in `La Latina`',
-          img: 'images/cities/latina.jpg'
-        },
-        {
-          label: 'Eating some tapas in `La Latina`',
-          img: 'images/cities/latina.jpg'
-        }
-      ],
-      cityOptions: ['Madrid', 'Bucharest', 'Lisbon']
-    },
-    {
+      {
+        label: 'Making a picnic in the Retiro Park',
+        img: 'images/cities/retiro.jpg',
+      },
+      {
+        label: 'Admiring the paintings in the Prado Museum',
+        img: 'images/cities/prado.jpg',
+      },
+      {
+        label: 'Eating some tapas in `La Latina`',
+        img: 'images/cities/latina.jpg',
+      },
+      {
+        label: 'Eating some tapas in `La Latina`',
+        img: 'images/cities/latina.jpg',
+      },
+    ],
+    cityOptions: ['Madrid', 'Bucharest', 'Lisbon'],
+  },
+  {
     name: 'Athens',
     police: 'Ελληνική Αστυνομία',
     policeimg: 'policeGR.png',
-	flag: 'GREC0001.GIF',	  		
-    food:'Glass of Ouzo',
-      hints: [
-        {
-          label: 'Walking around the Parthenon',
-          img: 'images/cities/ath_part.jpg'
-        },
-        {
-          label: 'Having a coffe at Plaka',
-          img: 'images/cities/ath_plaka.jpg'
-        },
-        {
-          label: 'Eating Souvlaki at Mikropolitiko',
-          img: 'images/cities/ath_souvlaki'
-        }
-      ],
-      cityOptions: ['Oslo', 'Athens', 'Dublin']
-    },
-    {
+    flag: 'countryFlags/GREC0001.GIF',
+    food: 'Glass of Ouzo',
+    hints: [
+      {
+        label: 'Walking around the Parthenon',
+        img: 'images/cities/ath_part.jpg',
+      },
+      {
+        label: 'Having a coffe at Plaka',
+        img: 'images/cities/ath_plaka.jpg',
+      },
+      {
+        label: 'Eating Souvlaki at Mikropolitiko',
+        img: 'images/cities/ath_souvlaki',
+      },
+    ],
+    cityOptions: ['Oslo', 'Athens', 'Dublin'],
+  },
+  {
     name: 'Amsterdam',
     police: 'POLITIE',
     policeimg: 'policeNL.png',
-	flag: 'NETH0001.GIF',	  		
-    food:'Heineken',
-      hints: [
-        {
-          label: 'Riding a bike along the canals and Vondelpark',
-          img: 'images/cities/vondelpark.jpg'
-        },
-        {
-          label: 'Visiting Anne Frank House',
-          img: 'images/cities/retiro.jpg'
-        },
-        {
-          label: 'Relaxing in some Coffee Shops',
-          img: 'images/cities/retiro.jpg'
-        }
-      ],
-      cityOptions: ['London', 'Amsterdam', 'Venice']
-    }
+    flag: 'countryFlags/NETH0001.GIF',
+    food: 'Heineken',
+    hints: [
+      {
+        label: 'Riding a bike along the canals and Vondelpark',
+        img: 'images/cities/vondelpark.jpg',
+      },
+      {
+        label: 'Visiting Anne Frank House',
+        img: 'images/cities/retiro.jpg',
+      },
+      {
+        label: 'Relaxing in some Coffee Shops',
+        img: 'images/cities/retiro.jpg',
+      },
+    ],
+    cityOptions: ['London', 'Amsterdam', 'Venice'],
+  },
 ];
