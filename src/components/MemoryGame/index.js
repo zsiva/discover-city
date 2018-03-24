4d9e9e5c9161e50607643ea3fe23e16032ee58f8
Marta Carrizo <m.carrizo.ibarra@gmail.com>
2018-03-24 09:45:54 +0100

    Update memory card game

--- src/components/MemoryGame/index.js
+++ src/components/MemoryGame/index.js
@@ -1,34 +1,54 @@
 import React, { Component, Fragment } from 'react';
-import { Grid } from 'semantic-ui-react';
-import CardIcon from './CardIcon';
-import NewCard from './NewCard';
+import { Grid, Button } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+import Card from './Card';
 import { shuffleArray } from '../../utils/operations';
+import Lightbox from '../Lightbox';
 
 const cardIcons = ['birthday', 'alarm', 'bug', 'paint brush', 'unhide', 'pin'];
 
 const cardList = shuffleArray([...cardIcons, ...cardIcons]);
 
-function initiateCards() {
+const initiateCards = () => {
   return cardList.map(card => ({
     value: card,
     matched: false,
     flipped: false,
   }));
-}
+};
 
-export default class Cards extends Component {
+export default class MemoryGame extends Component {
   constructor(props) {
     super(props);
     this.renderCards = this.renderCards.bind(this);
     this.checkMatch = this.checkMatch.bind(this);
     this.reset = this.reset.bind(this);
+    this.tick = this.tick.bind(this);
 
     this.state = {
       cards: initiateCards(),
       lastCard: null,
       locked: false,
       matches: 0,
+      timer: null,
+      counter: 20,
     };
+  }
+
+  componentDidMount() {
+    let timer = setInterval(this.tick, 1000);
+    this.setState({ timer });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.timer);
+  }
+
+  tick() {
+    this.setState({
+      counter: this.state.counter - 1,
+    });
   }
 
   checkMatch(value, id) {
@@ -64,7 +84,7 @@ export default class Cards extends Component {
     return cards.map((card, index) => {
       return (
         <Grid.Column key={index} mobile={8} tablet={4} computer={4}>
-          <NewCard
+          <Card
             key={index}
             value={card.value}
             id={index}
@@ -88,10 +108,37 @@ export default class Cards extends Component {
 
   render() {
     const numPairs = this.state.cards.filter(card => card.matched).length / 2;
+    if (this.state.counter === 0 || numPairs === this.state.cards.length / 2) {
+      this.refs.hintsLightbox.open();
+      clearInterval(this.state.timer);
+    }
+
     return (
       <Fragment>
-        <h5>Num of pairs found: {numPairs}</h5>
+        <h5>
+          Number of pairs found: {numPairs}
+          <div className="pull-right">Time Left: {this.state.counter}</div>
+        </h5>
         <Grid>{this.renderCards(this.state.cards)}</Grid>
+        <Lightbox
+          ref="hintsLightbox"
+          header={this.state.counter === 0 ? 'Your time is up!' : 'Great you found all the pairs'}
+        >
+          <Grid>
+            <Grid.Column width={8}>
+              <img src="./images/timeup.jpg" alt="time up" />
+            </Grid.Column>
+            <Grid.Column width={8}>
+              <p>Your time is up, you got {numPairs} correct answers.</p>
+              <p>{numPairs * 5} extra seconds</p>
+              <Link to="/user">
+                <Button color="green">
+                  <Button.Content content="Back to the profile" />
+                </Button>
+              </Link>
+            </Grid.Column>
+          </Grid>
+        </Lightbox>
       </Fragment>
     );
   }
--- src/components/Pages/GetMoney/index.js
+++ src/components/Pages/GetMoney/index.js
@@ -1,12 +1,9 @@
 //@flow
 import React, { Component, Fragment } from 'react';
 import { connect, type Dispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { Grid, Container, Statistic, Button } from 'semantic-ui-react';
-import Lightbox from '../../Lightbox';
+import { Grid, Container, Button } from 'semantic-ui-react';
 import { startTimer } from '../../../actions/timer';
-import Cards from '../../Card/Cards';
-import Game from '../../Card/Game';
+import MemoryGame from '../../MemoryGame';
 import Spinner from '../../Spinner';
 import Header from '../../Header';
 
@@ -17,27 +14,7 @@ export type GetMoneyPropType = {
   isLoading: boolean,
 };
 
-export type GetMoneyStateType = {
-  openModal: boolean,
-  correctAnswers: number,
-};
-
-class GetMoney extends Component<GetMoneyPropType, GetMoneyStateType> {
-  constructor(props: GetMoneyPropType) {
-    super(props);
-    this.state = {
-      openModal: false,
-      correctAnswers: 0,
-      previousAnswer: '',
-      flipped: false,
-    };
-  }
-
-  componentDidMount() {
-    const { dispatch } = this.props;
-    dispatch(startTimer());
-  }
-
+class GetMoney extends Component<GetMoneyPropType> {
   render() {
     const { timeRemaining, isLoading, gameEnded } = this.props;
 
@@ -51,37 +28,10 @@ class GetMoney extends Component<GetMoneyPropType, GetMoneyStateType> {
     return (
       <Fragment>
         <Header />
-        <header className="header">
-          <Statistic.Group widths="three" color="green" inverted size="small">
-            <Statistic>
-              <Statistic.Value>{this.state.correctAnswers}</Statistic.Value>
-              <Statistic.Label>Answers</Statistic.Label>
-            </Statistic>
-            <Statistic floated="right">
-              <Statistic.Value>{timeRemaining}</Statistic.Value>
-              <Statistic.Label>sec</Statistic.Label>
-            </Statistic>
-          </Statistic.Group>
-        </header>
+
         <Container>
-          <Game />
+          <MemoryGame />
         </Container>
-        <Lightbox ref="hintsLightbox" header="Your time is up!">
-          <Grid>
-            <Grid.Column width={8}>
-              <img src="./images/timeup.jpg" alt="time up" />
-            </Grid.Column>
-            <Grid.Column width={8}>
-              <p>Your time is up, you got {this.state.correctAnswers} correct answers.</p>
-              <p>{this.state.correctAnswers * 5} extra seconds</p>
-              <Link to="/user">
-                <Button color="green">
-                  <Button.Content content="Back to the profile" />
-                </Button>
-              </Link>
-            </Grid.Column>
-          </Grid>
-        </Lightbox>
       </Fragment>
     );
   }
