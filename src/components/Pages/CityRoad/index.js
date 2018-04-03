e4f02aa1ca6dae37a161b11643b7eda19aca45e1
Marta Carrizo <m.carrizo.ibarra@gmail.com>
2018-04-03 23:17:24 +0200

    Add mob animation

--- src/components/Pages/CityRoad/index.js
+++ src/components/Pages/CityRoad/index.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
-import { Container, Grid } from 'semantic-ui-react';
+import { Container, Grid, Responsive } from 'semantic-ui-react';
 import Spinner from '../../Spinner';
 import { withRouter } from 'react-router';
 
@@ -16,7 +16,15 @@ class CityRoad extends Component {
       this.props.history.push(location);
     }, 2200);
   };
-
+  moveCarMob = e => {
+    const location = e.currentTarget.dataset.to;
+    document.getElementById('taxiCity').style.transform = `translateY(${
+      e.clientY
+    }px) rotate(90deg)`;
+    setTimeout(() => {
+      this.props.history.push(location);
+    }, 2200);
+  };
   render() {
     const { currentCity, isLoading } = this.props;
 
@@ -32,36 +40,56 @@ class CityRoad extends Component {
             Welcome to {currentCity.name}{' '}
             <img src={`./images/${currentCity.flag}`} alt="country flag" className="headerFlag" />
           </h1>
-          <Grid centered textAlign="center" verticalAlign="bottom">
-            <Grid.Column mobile={10} tablet={5} computer={5} className="text-center">
-              <p>Visit the police station to get some hints</p>
-              <div data-to="/police" onClick={this.moveCar}>
+          <Responsive minWidth={768}>
+            <Grid centered textAlign="center" verticalAlign="bottom">
+              <Grid.Column mobile={10} tablet={5} computer={5} className="text-center">
+                <div data-to="/police" onClick={this.moveCar}>
+                  <div className="policeName">{currentCity.police}</div>
+                  <img src="./images/police.png" alt="police department" />
+                </div>
+              </Grid.Column>
+              <Grid.Column mobile={10} tablet={5} computer={5} className="text-center">
+                <div data-to="/airport" onClick={this.moveCar}>
+                  <div className="policeName">Airport</div>
+                  <img src="./images/airport.png" alt="Airport" />
+                </div>
+              </Grid.Column>
+              <Grid.Column mobile={10} tablet={5} computer={5} className="text-center">
+                <div data-to="/get-money" onClick={this.moveCar}>
+                  <div className="policeName">Casino</div>
+                  <img src="./images/casino.png" alt="casino" />
+                </div>
+              </Grid.Column>
+            </Grid>
+          </Responsive>
+          <Responsive as={Container} maxWidth={767}>
+            <div className="mobileContainer">
+              <div className="verticalImg" data-to="/police" onClick={this.moveCarMob}>
                 <div className="policeName">{currentCity.police}</div>
                 <img src="./images/police.png" alt="police department" />
               </div>
-            </Grid.Column>
-            <Grid.Column mobile={10} tablet={5} computer={5} className="text-center">
-              <p>Visit the airport to go to a different city</p>
-              <div data-to="/airport" onClick={this.moveCar}>
-                <div className="policeName">Airport</div>
-                <img src="./images/airport.png" alt="Airport" />
+              <div className="verticalImg" data-to="/airport" onClick={this.moveCarMob}>
+                <div className="policeName">{currentCity.police}</div>
+                <img src="./images/airport.png" alt="airport" />
               </div>
-            </Grid.Column>
-            <Grid.Column mobile={10} tablet={5} computer={5} className="text-center">
-              <p>Earn some money to pay expenses</p>
-              <div data-to="/get-money" onClick={this.moveCar}>
-                <div className="policeName">Casino</div>
+              <div className="verticalImg" data-to="/get-money" onClick={this.moveCarMob}>
+                <div className="policeName">{currentCity.police}</div>
                 <img src="./images/casino.png" alt="casino" />
               </div>
-            </Grid.Column>
-          </Grid>
+              <div className="roadVertical">
+                <div className="verticalDivider" />
+                <img id="taxiCity" src="./images/transport/car.png" alt="car" className="taxiMob" />
+              </div>
+            </div>
+          </Responsive>
         </Container>
-        <div className="roadContainer">
-          <img id="taxiCity" src="./images/transport/car.png" alt="car" width="70" height="36" />
-          <div className="roadDivider" />
-
-          <div className="road" />
-        </div>
+        <Responsive minWidth={768}>
+          <div className="roadContainer">
+            <img id="taxiCity" src="./images/transport/car.png" alt="car" />
+            <div className="roadDivider" />
+            <div className="road" />
+          </div>
+        </Responsive>
       </Fragment>
     );
   }
