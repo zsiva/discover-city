1deddd95d56132f07d9a733c03016b01cb387e6f
Marta Carrizo <m.carrizo.ibarra@gmail.com>
2018-04-15 19:41:58 +0200

    Add police link to menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 //@flow
 import React from 'react';
-import { Icon, Menu } from 'semantic-ui-react';
+import { Icon, Menu, Container } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 import './style.css';
@@ -8,30 +8,40 @@ import './style.css';
 export default function Header() {
   return (
     <Menu inverted icon="labeled">
-      <Link to="/city">
-        <Menu.Item active={window.location.pathname === '/city'}>
-          <Icon name="home" />
-          City
-        </Menu.Item>
-      </Link>
-      <Link to="/airport">
-        <Menu.Item active={window.location.pathname === '/airport'}>
-          <Icon name="plane" />
-          Airport
-        </Menu.Item>
-      </Link>
-      <Link to="/get-money">
-        <Menu.Item active={window.location.pathname === '/get-money'}>
-          <Icon name="money" />
-          Casino
-        </Menu.Item>
-      </Link>
-      <Link to="/user">
-        <Menu.Item active={window.location.pathname === '/user'}>
-          <Icon name="user" />
-          Profile
-        </Menu.Item>
-      </Link>
+      <Container>
+        <Link to="/city">
+          <Menu.Item active={window.location.pathname === '/city'}>
+            <Icon name="home" size="small" />
+            City
+          </Menu.Item>
+        </Link>
+        <Link to="/airport">
+          <Menu.Item active={window.location.pathname === '/airport'}>
+            <Icon name="plane" />
+            Airport
+          </Menu.Item>
+        </Link>
+        <Link to="/police">
+          <Menu.Item active={window.location.pathname === '/police'}>
+            <Icon name="building outline" />
+            Police
+          </Menu.Item>
+        </Link>
+        <Link to="/get-money">
+          <Menu.Item active={window.location.pathname === '/get-money'}>
+            <Icon name="money" />
+            Casino
+          </Menu.Item>
+        </Link>
+        <Menu.Menu position="right">
+          <Link to="/user">
+            <Menu.Item active={window.location.pathname === '/user'}>
+              <Icon name="user" />
+              Profile
+            </Menu.Item>
+          </Link>
+        </Menu.Menu>
+      </Container>
     </Menu>
   );
 }
