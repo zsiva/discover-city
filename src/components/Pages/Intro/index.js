dcd33820364ab32063dede2dc7741748697997ae
Marta Carrizo <m.carrizo.ibarra@gmail.com>
2018-05-06 20:16:55 +0200

    Ask player info on first screen

--- src/components/Pages/Intro/index.js
+++ src/components/Pages/Intro/index.js
@@ -1,58 +1,136 @@
-import React, { Fragment } from 'react';
-import { Button, Responsive } from 'semantic-ui-react';
+//@flow
+import React, { Fragment, Component } from 'react';
+import { connect, type Dispatch } from 'react-redux';
+import { Button, Responsive, Form } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import { setName, setLanguage } from '../../../actions/player';
 import './style.css';
 
-export default function Intro() {
-  return (
-    <Fragment>
-      <section className="ui container">
-        <div className="intro">
-          <Responsive as="h1" maxWidth={480}>
-            Help O&apos;Greeny
-          </Responsive>
-          <img className="leprechaun" src="./images/ogreeny2.png" alt="ogreeny" />
-          <Responsive as="h1" minWidth={481}>
-            Help O&apos;Greeny
-          </Responsive>
-          <p className="text-left">
-            Our little lepprechaun O&apos;Greeny was chilling, having a cold Guiness while his money
-            was stolen.
-          </p>
-          <p className="text-left">
-            He has been looking all over the world for it but cannot find it. Help him!
-          </p>
-          <br />
-          <Link to="/user">
-            <Button content="Start game" color="green" />
-          </Link>
-        </div>
-      </section>
+export type IntroPropType = {
+  dispatch: Dispatch,
+};
 
-      <footer className="container">
-        <small>
-          Images by Freepik: &nbsp;
-          <a
-            rel="noopener noreferrer"
-            href="http://www.freepik.com/free-vector/four-leaf-clover-background_765853.htm"
-          >
-            cards
-          </a>&nbsp;
-          <a
-            rel="noopener noreferrer"
-            href="http://www.freepik.com/free-vector/alarm-clock_796418.htm"
-          >
-            Clock
-          </a>&nbsp;
-          <a
-            rel="noopener noreferrer"
-            target="_blank"
-            href="http://www.freepik.com/free-vector/st-patrick-s-background-design_1064485.htm"
-          >
-            Leprechaun
-          </a>
-        </small>
-      </footer>
-    </Fragment>
-  );
+export type IntroStateType = {
+  name: string,
+  language: string,
+  displayIntro: boolean,
+};
+
+const languageOptions = [
+  {
+    text: 'English',
+    value: 'en',
+    image: { src: './images/countryFlags/UNKG0001.GIF' },
+  },
+  {
+    text: 'Español',
+    value: 'es',
+    image: { src: './images/countryFlags/SPAN0001.GIF' },
+  },
+];
+
+class Intro extends Component<IntroPropType, IntroStateType> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      language: '',
+      displayIntro: false,
+    };
+  }
+
+  saveName = (e, { value }) => {
+    this.props.dispatch(setName(this.state.name));
+    this.props.dispatch(setLanguage(this.state.language));
+    this.setState({ displayIntro: true });
+  };
+
+  handleChange = (e, { value }) => this.setState({ name: value });
+  handleChangeLanguage = (e, { value }) => this.setState({ language: value });
+
+  render() {
+    return (
+      <Fragment>
+        <section className="ui container">
+          <div className="intro">
+            <Responsive as="h1" maxWidth={480}>
+              Help O&apos;Greeny
+            </Responsive>
+            <img className="leprechaun" src="./images/ogreeny2.png" alt="ogreeny" />
+            <Responsive as="h1" minWidth={481}>
+              Help O&apos;Greeny
+            </Responsive>
+            {!this.state.displayIntro && (
+              <Form fluid onSubmit={this.saveName} className="nameForm">
+                <Form.Input
+                  type="text"
+                  icon="user"
+                  iconPosition="left"
+                  placeholder="Your name"
+                  value={this.state.name}
+                  onChange={this.handleChange}
+                />
+                <Form.Select
+                  placeholder="Select your language"
+                  options={languageOptions}
+                  onChange={this.handleChangeLanguage}
+                  value={this.state.language}
+                />
+                <Form.Button content="Save" color="green" size="large" fluid />
+              </Form>
+            )}
+            {this.state.displayIntro && (
+              <Fragment>
+                <p className="text-left">
+                  Our little lepprechaun O&apos;Greeny was chilling, having a cold Guiness while his
+                  money was stolen.
+                </p>
+                <p className="text-left">
+                  He has been looking all over the world for it but cannot find it. Help him!
+                </p>
+                <br />
+                <Link to="/user">
+                  <Button content="Start game" color="green" />
+                </Link>
+              </Fragment>
+            )}
+          </div>
+        </section>
+
+        <footer className="container">
+          <small>
+            Images by Freepik: &nbsp;
+            <a
+              rel="noopener noreferrer"
+              href="http://www.freepik.com/free-vector/four-leaf-clover-background_765853.htm"
+            >
+              cards
+            </a>&nbsp;
+            <a
+              rel="noopener noreferrer"
+              href="http://www.freepik.com/free-vector/alarm-clock_796418.htm"
+            >
+              Clock
+            </a>&nbsp;
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href="http://www.freepik.com/free-vector/st-patrick-s-background-design_1064485.htm"
+            >
+              Leprechaun
+            </a>
+          </small>
+        </footer>
+      </Fragment>
+    );
+  }
 }
+
+const mapStateToProps = (state, ownProps = {}) => {
+  return {
+    playerName: state.player.name,
+    playerLanguage: state.player.language,
+  };
+};
+
+export default connect(mapStateToProps)(Intro);
--- src/components/Pages/Profile/index.js
+++ src/components/Pages/Profile/index.js
@@ -3,149 +3,89 @@ import React, { Component, Fragment } from 'react';
 import { Button, List, Label, Segment, Grid, Form, Select } from 'semantic-ui-react';
 import { connect, type Dispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { setName, setLanguage } from '../../../actions/player';
 import { findTextLang } from '../../../utils/findTextLang';
 import Header from '../../Header';
 import './style.css';
 
-const friendOptions = [
-   {
-     text: 'English',
-     value: 'en',
-	 image: { src: './images/countryFlags/UNKG0001.GIF' },
-	 },
-     {
-	 text: 'Español',
-     value: 'es',
-	 image: { src: './images/countryFlags/SPAN0001.GIF' },
-   }
- ]
 export type ProfilePropType = {
   dispatch: Dispatch,
   timeRemaining: number,
   moneyLeft: number,
   playerName: string,
+  playerLanguage: string,
 };
 
-export type ProfileStateType = {
-  name: string,
-};
+function Profile(props: ProfilePropType) {
+  let { timeRemaining, moneyLeft, playerName, playerLanguage } = props;
+  timeRemaining = timeRemaining < 0 ? 0 : timeRemaining;
 
-class Profile extends Component<ProfilePropType, ProfileStateType> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-	  language: '',
-    };
-  }
+  return (
+    <Fragment>
+      <Header />
+      <section className="ui container">
+        <h1>
+          {findTextLang(playerLanguage, 'profile_hello')} {playerName}
+        </h1>
+        <Grid>
+          <Grid.Column mobile={16} tablet={8} computer={8}>
+            <Segment raised>
+              <Label color="green" ribbon>
+                {findTextLang(playerLanguage, 'profile_profile')}
+              </Label>
+              <List>
+                <List.Item>
+                  <List.Icon name="star" color="green" />
+                  <List.Content>{findTextLang(playerLanguage, 'profile_points')} 0</List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Icon color="green" name="clock" />
+                  <List.Content>
+                    {findTextLang(playerLanguage, 'profile_time')} {timeRemaining}
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Icon color="green" name="money" />
+                  <List.Content>
+                    {findTextLang(playerLanguage, 'profile_money')} {moneyLeft}
+                  </List.Content>
+                </List.Item>
+              </List>
+            </Segment>
+          </Grid.Column>
 
-  handleChange = (e, { value }) => this.setState({ name: value });
-  handleChangeL = (e, { value }) => this.setState({ language: value });
+          <Grid.Column mobile={16} tablet={8} computer={8}>
+            <Segment>
+              <Label color="green" ribbon>
+                {findTextLang(playerLanguage, 'profile_items')}
+              </Label>
+            </Segment>
+          </Grid.Column>
+        </Grid>
 
-  saveName = (e, { value }) => {
-	this.props.dispatch(setName(this.state.name));
-	this.props.dispatch(setLanguage(this.state.language));
-	};
-
-  render() {
-    let { timeRemaining, moneyLeft, playerName, playerLanguage } = this.props;
-    timeRemaining = timeRemaining < 0 ? 0 : timeRemaining;
-    const displayIntro = this.props.playerName === '';
-	//const displayList = this.props.playerLanguage === '';
-    return (
-      <Fragment>
-        <Header />
-        <section className="ui container">
-          {displayIntro && (
-            <Fragment>
-              <p>
-                You are now a detective and need to extract clues from the police departments on
-                each city to find out the thief s whereabouts.
-              </p>
-              <p>Go to the rules page to checkout all the instructions.</p>
-              <Form onSubmit={this.saveName}>
-                <Form.Group>
-                  <Form.Input
-                    type="text"
-                    icon="user"
-                    iconPosition="left"
-                    placeholder="Your name"
-                    value={this.state.name}
-                    onChange={this.handleChange}
-                  />
-
-                  <Form.Button content="Save" />
-                </Form.Group>
-              </Form>
-
-
-			<Select placeholder='Select your language' options={friendOptions}
-			onChange={this.handleChangeL}
-			value={this.state.language}
-			/>
-
-            </Fragment>
-          )}
-          {!displayIntro && (
-            <Fragment>
-              <h1>{findTextLang(playerLanguage,'profile_hello')} {playerName}</h1>
-              <Grid>
-                <Grid.Column mobile={16} tablet={8} computer={8}>
-                  <Segment raised>
-                    <Label color="green" ribbon>
-                      {findTextLang(playerLanguage,'profile_profile')}
-                    </Label>
-                    <List>
-                      <List.Item>
-                        <List.Icon name="star" color="green" />
-                        <List.Content>{findTextLang(playerLanguage,'profile_points')}  0</List.Content>
-                      </List.Item>
-                      <List.Item>
-                        <List.Icon color="green" name="clock" />
-                        <List.Content>{findTextLang(playerLanguage,'profile_time')} {timeRemaining}</List.Content>
-                      </List.Item>
-                      <List.Item>
-                        <List.Icon color="green" name="money" />
-                        <List.Content>{findTextLang(playerLanguage,'profile_money')} {moneyLeft}</List.Content>
-                      </List.Item>
-                    </List>
-                  </Segment>
-                </Grid.Column>
-
-                <Grid.Column mobile={16} tablet={8} computer={8}>
-                  <Segment>
-                    <Label color="green" ribbon>
-                      {findTextLang(playerLanguage,'profile_items')}
-                    </Label>
-
-                  </Segment>
-                </Grid.Column>
-              </Grid>
-
-              <Grid className="actions">
-                <Grid.Column mobile={8} tablet={4} computer={4}>
-                  <Link to="/get-money">
-                    <Button color="green" content={findTextLang(playerLanguage,'profile_earnMoney')} fluid />
-                  </Link>
-                </Grid.Column>
-                <Grid.Column mobile={8} tablet={4} computer={4}>
-                  <Link to="/rules">
-                    <Button color="green" content={findTextLang(playerLanguage,'profile_rules')} fluid />
-                  </Link>
-                </Grid.Column>
-                <Grid.Column mobile={8} tablet={4} computer={4}>
-                  <Link to="/city">
-                    <Button content={findTextLang(playerLanguage,'profile_find')} color="green" fluid />
-                  </Link>
-                </Grid.Column>
-              </Grid>
-            </Fragment>
-          )}
-        </section>
-      </Fragment>
-    );
-  }
+        <Grid className="actions">
+          <Grid.Column mobile={8} tablet={4} computer={4}>
+            <Link to="/get-money">
+              <Button
+                color="green"
+                content={findTextLang(playerLanguage, 'profile_earnMoney')}
+                fluid
+              />
+            </Link>
+          </Grid.Column>
+          <Grid.Column mobile={8} tablet={4} computer={4}>
+            <Link to="/rules">
+              <Button color="green" content={findTextLang(playerLanguage, 'profile_rules')} fluid />
+            </Link>
+          </Grid.Column>
+          <Grid.Column mobile={8} tablet={4} computer={4}>
+            <Link to="/city">
+              <Button content={findTextLang(playerLanguage, 'profile_find')} color="green" fluid />
+            </Link>
+          </Grid.Column>
+        </Grid>
+      </section>
+    </Fragment>
+  );
 }
 
 const mapStateToProps = (state, ownProps = {}) => {
