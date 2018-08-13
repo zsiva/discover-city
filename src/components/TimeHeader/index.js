876627ff1e1b230c955184ee4a05944d9aa6add2
Marta <m.carrizo.ibarra@gmail.com>
2018-08-13 17:07:32 +0200

    Create block for the header

--- src/components/TimeHeader/index.js
+++ src/components/TimeHeader/index.js
@@ -9,15 +9,16 @@ const TimeHeader = props => {
   const time = calculateDay(props.dateTime);
   return (
     <Fragment>
-      <div className="imageWrapper">
-        <img src={`./images/${props.currentCity.flag}`} alt="country flag" />
-        <FormattedMessage id={`cities.${props.currentCity.name}.name`} />
+      <div className="topWrapper">
+        <div className="imageWrapper">
+          <img src={`./images/${props.currentCity.flag}`} alt="country flag" />
+          <FormattedMessage id={`cities.${props.currentCity.name}.name`} />
+        </div>
+        <div className="timeWrapper">
+          <Icon name="clock" />
+          <FormattedMessage id={`week_days.${time.weekDay}`} />, {time.hDisplay}
+        </div>
       </div>
-      <div className="timeWrapper">
-        <Icon name="clock" />
-        <FormattedMessage id={`week_days.${time.weekDay}`} />, {time.hDisplay}
-      </div>
-      <br />
       <h1 className="text-center">
         <FormattedMessage id={props.messageId} values={props.messageValues} />
       </h1>
